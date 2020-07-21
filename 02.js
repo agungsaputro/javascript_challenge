76d9fb64e81695585ac23e773c43fd4337d27ca8
agungsaputro <35617674+agungsaputro@users.noreply.github.com>
2020-07-21 20:04:20 +0700

    Create 02.js

---
+++ 02.js
@@ -0,0 +1,7 @@
+const first = ['Behind', 'every', 'great', 'man']
+const second = ['is', 'a', 'woman']
+const third = ['rolling', 'her', 'eyes']
+
+const combineArray = [...first, ...second, ...third]
+
+console.log(combineArray)

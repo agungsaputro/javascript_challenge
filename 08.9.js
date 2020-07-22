7383ece8c8d24d606af915bdba78fcf60f7787aa
agungsaputro <35617674+agungsaputro@users.noreply.github.com>
2020-07-22 07:40:29 +0700

    Create 08.9.js

---
+++ 08.9.js
@@ -0,0 +1,8 @@
+function countWords(str) {
+    str = str.replace(/(^\s*)|(\s*$)/gi,"");
+    str = str.replace(/[ ]{2,}/gi," ");
+    str = str.replace(/\n /,"\n");
+    return str.split(' ').length;
+ }
+
+ console.log(countWords("lorem ipsum"))
